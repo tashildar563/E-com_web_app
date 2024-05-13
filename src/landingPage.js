90734995b0ac63751f87523d93eaf81762386b5b
Rakesh1186 <rakesh.tashildar@trustt.com>
2024-05-14 00:57:15 +0530

    landing page : images added

--- src/landingPage.js
+++ src/landingPage.js
@@ -7,20 +7,32 @@ import Container from 'react-bootstrap/Container';
 import GridLayout from "react-grid-layout";
 import SplitPane, { Pane } from 'react-split-pane';
 import './CSS/LandingPageModule.css';
+import { Draggable } from 'react-beautiful-dnd';
 
 
 
 const LandingPage = () => {
     return (
-        <div class="row">
-            <div class="column _25">
-                25% Left Sidebar
+        <div className="row">
+            <div className="inner-row" style={{ fontFamily: 'monospace' }}>
+                inner row
             </div>
-            <div class="column _55">
-                55% Main Content
+            <div className="menu-row" style={{ fontFamily: 'monospace' }}>
+                inner row
             </div>
-            <div class="column _20">
-                20% Right Sidebar
+            <div className="main-area">
+                <SplitPane split="vertical" minSize={1300} defaultSize={1300} maxSize={1700}>
+                    <div className="grid-row" style={{ backgroundColor: '#a4c639', height: '100vh' }}>
+                        {Array.from({ length: 6 }, (_, index) => (
+                            <div className="inner-column" key={index}>
+                                <h2>Column {index + 1}</h2>
+                                <p>Some text..</p>
+                                <img src="/logo192.png" style={{ width: '50%', height: 'auto' }} />
+                            </div>
+                        ))}
+                    </div>
+                    <div style={{ backgroundColor: '#ffbb00', height: '100vh' }}></div>
+                </SplitPane>
             </div>
         </div>
     );
