2a08b8ebb549ba48762095dab97bdb370f572ac8
Rakesh1186 <rakesh.tashildar@trustt.com>
2024-05-14 01:26:42 +0530

    landing page :adding button

--- src/landingPage.js
+++ src/landingPage.js
@@ -12,27 +12,35 @@ import { Draggable } from 'react-beautiful-dnd';
 
 
 const LandingPage = () => {
+    const columnNames = ['Sari 1', 'Sari 2', 'Sari 3', 'Sari 4', 'Sari 5', 'Sari 6'];
+
     return (
         <div className="row">
             <div className="inner-row" style={{ fontFamily: 'monospace' }}>
-                inner row
+                Tashildar
             </div>
-            <div className="menu-row" style={{ fontFamily: 'monospace' }}>
-                inner row
+            <Button className="btn" style={{ width: '40px' }}>Home</Button>
+            <div style={{ width: '100px', display:'flex' }}>
+                <Button className="btn" style={{ width: '100px', display:'flex' }}>Home</Button>
+                {/* <Button className="btn" style={{ width: '10%' }}>Purchase Order</Button>
+                <Button className="btn" style={{ width: '10%' }}>Stock</Button>
+                <Button className="btn" style={{ width: '10%' }}>Invoice</Button>
+                <Button className="btn" style={{ width: '10%' }}>Reports</Button>
+                <Button className="btn" style={{ width: '10%' }}>Settings</Button> */}
             </div>
             <div className="main-area">
-                <SplitPane split="vertical" minSize={1300} defaultSize={1300} maxSize={1700}>
-                    <div className="grid-row" style={{ backgroundColor: '#a4c639', height: '100vh' }}>
+                {/* <SplitPane split="vertical" minSize={1300} defaultSize={1300} maxSize={1700}> */}
+                    <div className="grid-row" style={{ backgroundColor: '#a4c639', height: '100vh', display: 'flex' }}>
                         {Array.from({ length: 6 }, (_, index) => (
-                            <div className="inner-column" key={index}>
-                                <h2>Column {index + 1}</h2>
+                            <div className="inner-column" key={index} style={{ width: '16.666%' }}>
+                                <h2>{columnNames[index]}</h2>
                                 <p>Some text..</p>
                                 <img src="/logo192.png" style={{ width: '50%', height: 'auto' }} />
                             </div>
                         ))}
                     </div>
-                    <div style={{ backgroundColor: '#ffbb00', height: '100vh' }}></div>
-                </SplitPane>
+                    {/* <div style={{ backgroundColor: '#ffbb00', height: '100vh' }}></div> */}
+                {/* </SplitPane> */}
             </div>
         </div>
     );
